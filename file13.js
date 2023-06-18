3ff1c2adb9621da7e0aa146f4eb05a5e11bcd7ed
NaganGowda <118706849+NaganGowda@users.noreply.github.com>
2023-06-18 23:19:32 +0530

    #

---
+++ file13.js
@@ -0,0 +1,11 @@
+//how javascript works
+//js compiles whole code before execution
+//means js looks at the whole code before execution
+
+//it happens in 3 steps 1=> is lexical analysis/Tokenization: The source code is broken down into individual tokens, such as keywords, identifiers, operators, and literals
+
+//2=>parsing:The tokens are then analyzed and structured into an abstract syntax tree (AST)
+//3=>Code Generation: The final step is to generate executable code from the optimized IR(intermediate representation) or AST.
+
+//why compilation before execution
+//early error checking and determining appropriate scope for variable
